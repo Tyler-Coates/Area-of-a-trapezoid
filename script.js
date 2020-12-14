ea0228e9c12ea8bce5a0b0f829f3be5a39fc6cde
TylerCoates <Tyler.coates@ocsbstudent.ca>
2020-12-14 14:42:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var trapezoidBase1 = parseInt(prompt("Please enter base1 of trapezoid:"));
+var trapezoidBase2 = parseInt(prompt("Please enter base2 of trapezoid:"));
+var trapezoidHeight = parseInt(prompt("Please enter height of trapezoid:"));
+var areaOfTrapezoid = (trapezoidBase1 + trapezoidBase2) * trapezoidHeight / 2;
+console.log("Area of trapezoid is: " + areaOfTrapezoid);
